7cc95939cf32d670005b3c05dcd4806882061cc3
Sandrine Ngo <sandrinengo@yahoo.com>
2021-01-08 11:06:34 -0500

    added new model named Contact

---
+++ Models/Contact.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Dotnet5Webapp.Models
+{
+    public class Contact
+    {
+        public int ID { get; set; }
+
+        public string FirstName { get; set; }
+
+        public string LastName { get; set; }
+
+        public string NickName { get; set; }
+
+        public string Place { get; set; }
+
+        public DateTime CreatedDate { get; set; } = DateTime.Now;
+
+        public bool isActive { get; set; }
+    }
+}
